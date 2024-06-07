35d21cabca97f3731d933086852b97977979bed3
Jaro95 <jmar95@o2.pl>
2024-06-07 12:12:14 +0200

    in progress

--- src/main/java/pl/coderslab/Workshop_5/Controller/BookController.java
+++ src/main/java/pl/coderslab/Workshop_5/Controller/BookController.java
@@ -1,5 +1,6 @@
 package pl.coderslab.Workshop_5.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.Workshop_5.Model.Book;
 import pl.coderslab.Workshop_5.Service.BookService;
@@ -7,7 +8,8 @@ import pl.coderslab.Workshop_5.Service.BookService;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping()
 public class BookController {
 
@@ -15,6 +17,11 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping("/start")
+    public String start(){
+        return "base";
     }
 
     @GetMapping("/helloBook")
---
+++ src/main/webapp/WEB-INF/jsp/base.jsp
@@ -0,0 +1,11 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 07.06.2024
+  Time: 10:37
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:include page="header.jsp" />
+<jsp:include page="footer.jsp" />
